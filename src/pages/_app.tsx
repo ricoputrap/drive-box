d2b7642e81df0be8b2bb32657ae7482201b137c4
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-04-02 21:32:31 +0700

    Setup initial configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = extendTheme({
+    fonts: {
+      body: `'Lato', sans-serif`
+    },
+    colors: {
+      primaryBlue: "#5053D9",
+      backgroundPrimary: "#ECEFF6",
+      backgroundSecondary: "#F0F0F0",
+      textPrimary: "#333333",
+      border: "#BDBDBD"
+    }
+  });
+
+  return (
+    <ChakraProvider theme={ theme }>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
