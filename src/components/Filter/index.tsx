c8b305c856de4585f27bafdb7fe14e5d47da16f1
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-04-09 10:35:32 +0700

    feat: implement a reusable <Filter /> component

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,43 @@
+import { TOption } from '@/types/file.types'
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react'
+import Select, { ActionMeta, MultiValue } from 'react-select'
+import customStyles from './customStyles';
+
+interface Props {
+  label: string;
+  values: MultiValue<TOption>;
+  handleChange: (newValue: MultiValue<TOption>, actionMeta: ActionMeta<TOption>) => void;
+  options: TOption[];
+}
+
+const Filter: React.FC<Props> = ({ label, values, handleChange, options }) => {
+  return (
+    <Box padding="20px" background="white" borderRadius="10px">
+      <Text
+        fontSize="20px"
+        fontWeight={700}
+        marginBottom="10px"
+        textTransform="uppercase"
+      >
+        { label }
+      </Text>
+
+      <Select
+        isMulti
+        placeholder=""
+        value={ values }
+        options={ options }
+        onChange={ handleChange }
+        components={{
+          DropdownIndicator:() => null,
+          IndicatorSeparator: () => null
+        }}
+        styles={ customStyles }
+        isClearable={ false }
+      />
+    </Box>
+  )
+}
+
+export default Filter
--- src/components/FilterTypes/index.tsx
+++ src/components/FilterTypes/index.tsx
@@ -1,9 +1,7 @@
 import { TOption } from '@/types/file.types';
 import { getFileTypeOptions } from '@/utils/options';
-import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
-import Select from 'react-select';
-import customStyles from './customStyles';
+import Filter from '../Filter';
 import useFilter from './useFilter';
 
 const OPTIONS: TOption[] = getFileTypeOptions();
@@ -12,25 +10,12 @@ const FilterTypes: React.FC = () => {
   const { types, handleTypesChange } = useFilter();
 
   return (
-    <Box padding="20px" background="white" borderRadius="10px">
-      <Text fontSize="20px" fontWeight={700} marginBottom="10px">
-        TYPES
-      </Text>
-
-      <Select
-        isMulti
-        placeholder=""
-        value={ types }
-        options={OPTIONS}
-        onChange={ handleTypesChange }
-        components={{
-          DropdownIndicator:() => null,
-          IndicatorSeparator: () => null
-        }}
-        styles={ customStyles }
-        isClearable={ false }
-      />
-    </Box>
+    <Filter
+      label="types"
+      values={ types }
+      handleChange={ handleTypesChange }
+      options={ OPTIONS }
+    />
   )
 }
 
